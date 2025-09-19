17ffdb63a30d2c3f17eb39ebce40b563c7327295
Joshua <joshua.vansant@outlook.com>
2025-09-19 14:17:40 -0600

    edited requirements for Render

--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-# import os
-# import psycopg2
-# from dotenv import load_dotenv
-# from flask import Flask, jsonify, request
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-# from flask_bcrypt import Bcrypt
-# from flask_jwt_extended import JWTManager, create_access_token
-# # from models.user import User
 from flask import Flask, jsonify, request
 import os
 from extensions import db, migrate, bcrypt, jwt
