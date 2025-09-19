a341ff615946e111ab303a0e86d2f61a6907d9b5
Joshua <joshua.vansant@outlook.com>
2025-09-18 21:45:43 -0600

    Revert "feat: setup postgres database with user model"
    This reverts commit eac1b46d9d8e2a355ca53d4aa78037fe39648a7d.

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-from models import User
 
 @app.get('/')
 def home():
     return 'Hello, Worlds!'
-
-@app.get('/users')
-def get_users():
-    users = User.query.all()
-    return {'users': [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]}
-
-@app.post('/users')
-def create_user():
-    from flask import request
-    data = request.get_json()
-    user = User(username=data['username'], email=data['email'])
-    db.session.add(user)
-    db.session.commit()
-    return {'message': 'User created successfully', 'user_id': user.id}
