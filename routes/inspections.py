7d3a7f37ffed56da65f2524ed773cf2c3314f737
Joshua <joshua.vansant@outlook.com>
2025-09-20 19:02:42 -0600

    added import

--- routes/inspections.py
+++ routes/inspections.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
 from models.inspection_results import InspectionResult
 from extensions import db, bcrypt
 from models.user import User
