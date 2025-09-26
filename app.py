100ced9bf101687908888ac9056a1e55eb4f1a27
Joshua <joshua.vansant@outlook.com>
2025-09-25 21:42:54 -0600

    moved app out of main block

--- app.py
+++ app.py
@@ -41,7 +41,9 @@ def create_app():
 
 
     return app
-# app = create_app()
+
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
+    # app = create_app()
     socketio.run(app, host="0.0.0.0", port=5000)
