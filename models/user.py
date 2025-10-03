099ec09d64704bbc0b7b71de0a96de0b0c74b150
Joshua <joshua.vansant@outlook.com>
2025-10-02 21:44:22 -0600

    more edits to user model

--- models/user.py
+++ models/user.py
@@ -16,7 +16,7 @@ class User(db.Model):
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
     phone_number = db.Column(db.String(15), nullable=True)
-    org = relationship("Organization", backref="users", lazy="joined")
+    org = relationship("Organization", backref="users", lazy="joined", foreign_keys=[org_id])
 
     def to_dict(self, include_org=True):
         data = {
